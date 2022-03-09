7fb99ac12fc65eb8e637d34b86ffe76b475d9b3c
Cole Webster <Cole.Webster@gmail.com>
2022-03-09 13:57:11 -0500

    Added custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'white': '#fffcf2',
+      'silver': '#ccc5b9',
+      'grey': '#403d39',
+      'black': '#252422',
+      'orange': '#eb5e28'
+    },
+
   },
   plugins: [],
 }
