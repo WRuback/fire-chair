3f3d814cee2d76d5416f9200d598d23db6d62eb0
EzequielMejiasMelo <ezequiel.mejiasmelo@gmail.com>
2022-03-10 13:11:47 -0500

    User Profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,29 +3,29 @@ import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import SkillsList from '../components/SkillsList';
-import SkillForm from '../components/SkillForm';
+import DeckList from '../components/DeckList';
+import DeckForm from '../components/DeckForm';
 
-import { QUERY_SINGLE_PROFILE, QUERY_ME } from '../utils/queries';
+import { QUERY_SINGLE_USER, QUERY_ME } from '../utils/queries';
 
 import Auth from '../utils/auth';
 
 const Profile = () => {
-  const { profileId } = useParams();
+  const { userId } = useParams();
 
   // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
   const { loading, data } = useQuery(
-    profileId ? QUERY_SINGLE_PROFILE : QUERY_ME,
+    userId ? QUERY_SINGLE_USER : QUERY_ME,
     {
-      variables: { profileId: profileId },
+      variables: { userId: userId },
     }
   );
 
   // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
-  const profile = data?.me || data?.profile || {};
+  const user = data?.me || data?.user || {};
 
   // Use React Router's `<Redirect />` component to redirect to personal profile page if username is yours
-  if (Auth.loggedIn() && Auth.getProfile().data._id === profileId) {
+  if (Auth.loggedIn() && Auth.getProfile().data._id === userId) {
     return <Navigate to="/me" />;
   }
 
@@ -33,7 +33,7 @@ const Profile = () => {
     return <div>Loading...</div>;
   }
 
-  if (!profile?.name) {
+  if (!user?.username) {
     return (
       <h4>
         You need to be logged in to see your profile page. Use the navigation
@@ -44,20 +44,19 @@ const Profile = () => {
 
   return (
     <div>
-      <h2 className="card-header">
-        {profileId ? `${profile.name}'s` : 'Your'} friends have endorsed these
-        skills...
-      </h2>
+      <h2>Your Custom Prompts</h2>
 
-      {profile.skills?.length > 0 && (
-        <SkillsList
-          skills={profile.skills}
-          isLoggedInUser={!profileId && true}
+      {user.deck?.length > 0 && (
+        <DeckList
+          deck={user.deck}
+          isLoggedInUser={!userId && true}
         />
       )}
 
+      <h2>Create a Prompt</h2>
+
       <div className="my-4 p-4" style={{ border: '1px dotted #1a1a1a' }}>
-        <SkillForm profileId={profile._id} />
+        <DeckForm profileId={user._id} />
       </div>
     </div>
   );
